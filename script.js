8a6fe591fecd49fbdfae9bde7a6ad1770a243bd7
rehamSWE <rehamalmowais@hotmail.com>
2026-05-04 15:04:05 +0300

    update form

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // 🔥 النطاق (Polygon)
 const polygon = [
-  { lat: 28.430263, lng: 45.974300 },
+  { lat: 28.430263, lng: 45.9743 },
   { lat: 28.431594, lng: 45.983996 },
   { lat: 28.420405, lng: 45.987023 },
-  { lat: 28.419397, lng: 45.975896 }
+  { lat: 28.419397, lng: 45.975896 },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-
   const button = document.getElementById("checkBtn");
   const message = document.getElementById("message");
 
   button.addEventListener("click", () => {
-
     // 🔥 الرسالة
     message.style.color = "#ffc107";
     message.textContent = "جارٍ تحديد الموقع .. يرجى السماح بالوصول عند الطلب";
@@ -34,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
           message.textContent = "تم التحقق بنجاح.";
 
           window.location.href =
-            "https://docs.google.com/forms/d/e/1FAIpQLSc7gTrd_dHVri96fGb5t63qB5ZesP4R5_n85YPxqLM54LnO5A/formResponse";
+            "https://docs.google.com/forms/d/e/1FAIpQLSc7gTrd_dHVri96fGb5t63qB5ZesP4R5_n85YPxqLM54LnO5A/viewform";
         } else {
           message.style.color = "#dc3545";
           message.textContent =
@@ -56,23 +54,23 @@ document.addEventListener("DOMContentLoaded", () => {
       {
         enableHighAccuracy: true,
         maximumAge: 0,
-      }
+      },
     );
   });
 });
-
 
 // 🔥 دالة التحقق داخل النطاق
 function isPointInPolygon(lat, lng, polygon) {
   let inside = false;
 
   for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-    const xi = polygon[i].lat, yi = polygon[i].lng;
-    const xj = polygon[j].lat, yj = polygon[j].lng;
+    const xi = polygon[i].lat,
+      yi = polygon[i].lng;
+    const xj = polygon[j].lat,
+      yj = polygon[j].lng;
 
     const intersect =
-      ((yi > lng) !== (yj > lng)) &&
-      (lat < (xj - xi) * (lng - yi) / (yj - yi) + xi);
+      yi > lng !== yj > lng && lat < ((xj - xi) * (lng - yi)) / (yj - yi) + xi;
 
     if (intersect) inside = !inside;
   }
